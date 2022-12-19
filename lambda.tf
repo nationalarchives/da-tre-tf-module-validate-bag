16551431ef57c7a463e2e1f8fe94b09b2a4c9079
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2022-12-19 16:54:06 +0000

    Use new ECR images; add tfvar parameter

--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "vb_bagit_checksum_validation" {
-  image_uri     = "${var.account_id}.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/tre-vb-validate-bagit:${var.vb_image_versions.tre_vb_validate_bagit}"
+  image_uri     = "${var.ecr_uri_host}/${var.ecr_uri_repo_prefix}${local.lambda_name_bag_validation}:${var.vb_image_versions.tre_vb_validate_bag}"
   package_type  = "Image"
-  function_name = local.lambda_name_bagit_validation
+  function_name = local.lambda_name_bag_validation
   role          = aws_iam_role.validate_bagit_lambda_invoke_role.arn
   timeout       = 30
 
@@ -13,7 +13,7 @@ resource "aws_lambda_function" "vb_bagit_checksum_validation" {
       "TRE_SYSTEM_NAME"          = upper(var.prefix)
       "TRE_PROCESS_NAME"         = local.step_function_name
       "TRE_STEP_FUNCTION_NAME"   = local.step_function_name
-      "TRE_LAMBDA_FUNCTION_NAME" = local.lambda_name_bagit_validation
+      "TRE_LAMBDA_FUNCTION_NAME" = local.lambda_name_bag_validation
       "TRE_ENVIRONMENT"          = var.env
     }
   }
@@ -24,9 +24,9 @@ resource "aws_lambda_function" "vb_bagit_checksum_validation" {
 }
 
 resource "aws_lambda_function" "vb_files_checksum_validation" {
-  image_uri     = "${var.account_id}.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/tre-vb-validate-bagit-files:${var.vb_image_versions.tre_vb_validate_bagit_files}"
+  image_uri     = "${var.ecr_uri_host}/${var.ecr_uri_repo_prefix}${local.lambda_name_bag_files_validation}:${var.vb_image_versions.tre_vb_validate_bagit_files}"
   package_type  = "Image"
-  function_name = local.lambda_name_files_validation
+  function_name = local.lambda_name_bag_files_validation
   role          = aws_iam_role.validate_bagit_lambda_invoke_role.arn
   timeout       = 30
 
@@ -38,7 +38,7 @@ resource "aws_lambda_function" "vb_files_checksum_validation" {
       "TRE_SYSTEM_NAME"          = upper(var.prefix)
       "TRE_PROCESS_NAME"         = local.step_function_name
       "TRE_STEP_FUNCTION_NAME"   = local.step_function_name
-      "TRE_LAMBDA_FUNCTION_NAME" = local.lambda_name_files_validation
+      "TRE_LAMBDA_FUNCTION_NAME" = local.lambda_name_bag_files_validation
       "TRE_ENVIRONMENT"          = var.env
     }
   }
@@ -51,7 +51,7 @@ resource "aws_lambda_function" "vb_files_checksum_validation" {
 
 # vb_step_function_trigger
 resource "aws_lambda_function" "vb_trigger" {
-  image_uri     = "${var.account_id}.dkr.ecr.eu-west-2.amazonaws.com/lambda_functions/tre-sqs-sf-trigger:${var.vb_image_versions.tre_sqs_sf_trigger}"
+  image_uri     = "${var.ecr_uri_host}${var.ecr_uri_repo_prefix}tre-sqs-sf-trigger:${var.vb_image_versions.tre_sqs_sf_trigger}"
   package_type  = "Image"
   function_name = local.lambda_name_trigger
   role          = aws_iam_role.vb_trigger_lambda.arn
