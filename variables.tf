0b436db89086ea2bdc709a39fe207b38b0773b5f
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2023-01-09 13:30:24 +0000

    Remove unused account_id

--- variables.tf
+++ variables.tf
@@ -8,12 +8,6 @@ variable "prefix" {
   type        = string
 }
 
-
-variable "account_id" {
-  description = "Account ID where Image for the Lambda function will be"
-  type        = string
-}
-
 variable "tre_data_bucket" {
   description = "TRE Data Bucket Name"
   type        = string
