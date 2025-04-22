d6c6d97f50923c60de013a3dd77e22d807239f57
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-04-22 08:32:50 +0100

    removing validate bag refs

--- variables.tf
+++ variables.tf
@@ -23,7 +23,6 @@ variable "vb_image_versions" {
   description = "Latest version of Images for Lambda Functions"
   type = object({
     tre_sqs_sf_trigger          = string
-    tre_vb_validate_bagit       = string
     tre_vb_validate_bagit_files = string
   })
 }
