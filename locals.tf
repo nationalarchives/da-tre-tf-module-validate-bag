c2f7d3a2bd3101e84a77fcf36e05bb69341ce6c3
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-04-22 09:34:44 +0100

    remove bag_validation

--- locals.tf
+++ locals.tf
@@ -1,7 +1,6 @@
 locals {
   resource_prefix                  = "${var.env}-${var.prefix}"
   step_function_name               = "${local.resource_prefix}-validate-bagit"
-  lambda_name_bag_validation       = "${local.resource_prefix}-vb-bag-validation"
   lambda_name_bag_files_validation = "${local.resource_prefix}-vb-bag-files-validation"
   lambda_name_trigger              = "${local.resource_prefix}-vb-trigger"
 }
